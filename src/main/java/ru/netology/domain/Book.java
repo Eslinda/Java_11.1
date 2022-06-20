2013abc51f95155998dd725b6aa900492537cedd
Tatiana Lyapina <antipts@gmail.com>
2022-06-20 23:15:29 +0300

    Remove unused array in the ProductManager (Rich Model)

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -3,11 +3,10 @@ package ru.netology.domain;
 public class Book extends Product {
     private String author;
 
-    public Book(int id, String name, int price, String author) {
+    public Book (int id, String name, int price, String author) {
         super(id, name, price);
         this.author = author;
     }
-
 
     @Override
     public boolean matches(String search) {
